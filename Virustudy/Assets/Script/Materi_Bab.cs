ddc403fca614a13e943a8986c859679cdab8121d
FeryEka <Ferrh.mahendra@gmail.com>
2022-09-28 19:11:25 +0700

    Tambah 3D, Materi Bab1, skor scene
    skore scene belum selesai

--- Virustudy/Assets/Script/Materi_Bab.cs
+++ Virustudy/Assets/Script/Materi_Bab.cs
@@ -8,7 +8,17 @@ public class Materi_Bab : MonoBehaviour
     public void BtnMateri_Bab1() {
         SceneManager.LoadScene("Materi_Bab1");
 	}
-	
+
+	public void BtnMateri_Bab2()
+	{
+		SceneManager.LoadScene("Materi_Bab2");
+	}
+
+	public void BtnMateri_Bab3()
+	{
+		SceneManager.LoadScene("Materi_Bab3");
+	}
+
 	public void BtnClose() {
         SceneManager.LoadScene("Menu");
 	}
